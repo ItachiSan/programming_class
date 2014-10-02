e00282e6618ba75403ab0f40d747c9d853981609
Giovanni Santini <giovannisantini93@yahoo.it>
2014-10-02 16:09:51 +0200

    Finished exercises 3 of 1st lesson While derping at DB lab

--- algorithms/1/es_3_print-figures.c
+++ algorithms/1/es_3_print-figures.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(void){
     int k,t;
-    char c='0',temp_c;
+    char c='0';
     printf("Cifrario di Cesare\n");
     printf("Inserire la frase: ");
-    scanf("%i", &k)
+    scanf("%i ", &k);
     k=k%26; // Evitiamoci problemi
-    while(c != '.'){
-        c=getchar();
-        temp_c=c;
-        if (temp_c >= 'A' && temp_c <= 'Z'){
-            if ((temp_c+k) > 'Z')
-                c= ('Z'-c)
+    while( (c=getchar()) != '.' && c != '\n'){
+        t=tolower(c) - 'a';
+	t=(t+k)%26;
+	if(c >= 'a' && c <= 'z') c='a'+t;
+	if(c >= 'A' && c <= 'Z') c='A'+t;
+	putchar(c);
         }
-        }
+    printf("\n");
+    return 0;
 }
