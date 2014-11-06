bbfc180200d550f1bbad23ca657126690d776b61
Giovanni Santini <giovannisantini93@yahoo.it>
2014-11-06 17:01:09 +0100

    Fixed some memory stuff in yesterday exercises Even if I'm @ DB lesson, uops

--- algorithms/5/es_3_2_2_rovescia-v2.c
+++ algorithms/5/es_3_2_2_rovescia-v2.c
@@ -4,19 +4,20 @@
 #include <math.h>
 
 int main(int argc, char *argv[]){
-    int i=-1,j=0,k=0,*num;
+    int i=-1,j=0,k=2,*num;
     num=malloc(2*sizeof(int));
     while(i != 0) {
         scanf(" %i", &i);
-        num[(k*2)+(j++)]=i;
-        if(j == 2){
-            num=realloc(num, (++k)*2*sizeof(int));
-            j=0;
+        num[j++]=i;
+        if(j == k){
+            k+=2;
+            num=realloc(num, k*sizeof(int));
         }
     }
     
-    for(i=k*2+j;i >=0; i--)
-        printf("%i ", num[i]);
+    for(i=j;i >= 0; i--)
+        if(num[i] != 0)
+            printf("%i ", num[i]);
     printf("\n");
     return EXIT_SUCCESS;
 }
