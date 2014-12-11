eed4d967e38465f0ff169c6c06aff8b374084f72
Giovanni Santini <giovannisantini93@yahoo.it>
2014-12-11 15:32:01 +0100

    Starting cookies, yuppie

---
+++ databases/stuff/9/cookie_session/counter.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include('../lib/header.php')?>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Esercizi BDLab</title>
+</head>
+
+<body>
+    <h1 class="uk-article-title">Contatore con i cookie</h1>
+    <?php include('../lib/navigation.php')?>
+    
+    <div class="uk-panel uk-panel-box uk-panel-box-primary uk-margin">
+<?php // Here comes the counter
+if(isset($_GET['mod']) && $_GET['mod']=="sum1"){
+    if(isset($_COOKIE['visit'])){
+        $counter=$_COOKIE['visit'];
+        $counter++;
+    } else {
+        $counter=1;
+    }
+if(isset($_GET['mod']) && $_GET['mod']=="set0"){
+    $counter=0;
+}
+    setcookie('visit', $counter, time()+120);
+}
+echo $counter;
+?>    
+    </div>
+    
+    <div>
+        <a href="counter.php?mod=sum1">Incrementa il contatore</a>
+        <a href="counter.php?mod=set0">Azzera il contatore</a>
+    </div>
+
+</body>
+
+</html>
