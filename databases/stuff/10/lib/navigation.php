9ef0d7b2fea145ff8623e756faa4173baaf6440b
Giovanni Santini <giovannisantini93@yahoo.it>
2014-12-18 15:50:16 +0100

    Updated the navigation bar, gonna create a function for connecting to PostgreSQL

--- databases/stuff/10/lib/navigation.php
+++ databases/stuff/10/lib/navigation.php
@@ -25,6 +25,18 @@
                 </ul>
             </div>
         </li>
+
+        <li class="uk-parent" data-uk-dropdown>
+            <a href="">Basi di dati</a>
+
+            <div class="uk-dropdown uk-dropdown-navbar submenu">
+                <ul class="uk-nav uk-nav-navbar">
+                    <li><a href="../dbms/products.php">Prodotti</a></li>
+                    <li><a href="../dbms/products_err.php">Prodotti (SQL injection)</a></li>
+                    <li><a href="../dbms/products_fix.php">Prodotti (corretta)</a></li>
+                </ul>
+            </div>
+        </li>
         
     </ul>
 
