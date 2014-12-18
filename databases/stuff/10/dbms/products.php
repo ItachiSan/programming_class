3316b8f4d3973dfaa39cec7a258dca778e3f8b79
Giovanni Santini <giovannisantini93@yahoo.it>
2014-12-18 16:04:17 +0100

    Added initial connection stuff

---
+++ databases/stuff/10/dbms/products.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php include('../lib/header.php')?>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Esercizi BDLab</title>
+</head>
+
+<body>
+    <h1 class="uk-article-title">Accesso alle basi di dati</h1>
+    <?php include('../lib/navigation.php')?>
+    <?php include('../lib/functions.php')?>
+    
+
+<?php
+// Apriamo la connessione
+$db = open_pg_connection();
+
+?>
+
+
+</body>
+
+</html>
--- databases/stuff/10/lib/functions.php
+++ databases/stuff/10/lib/functions.php
@@ -98,4 +98,9 @@ function show_login_form(){
     </form>
     ');
 }
+
+function open_pg_connection(){
+    $connection = "host=".myhost." dbname=".mydb." user=".myuser." password".mypsw;
+    return pg_connect($connection);
+}
 ?>
