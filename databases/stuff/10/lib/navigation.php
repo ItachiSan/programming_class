35f76fc8781a2c5e2e1cf9bb3e27f30e5c787270
Giovanni Santini <giovannisantini93@yahoo.it>
2014-12-18 15:40:41 +0100

    Started a new lesson Rewritten login_cookie, now login_session

---
+++ databases/stuff/10/lib/navigation.php
@@ -0,0 +1,34 @@
+<nav class="uk-navbar">
+
+    <ul class="uk-navbar-nav">
+        <li><a href="../index.php">Pagina principale</a></li>
+        <li class="uk-parent" data-uk-dropdown>
+            <a href="">Parametri</a>
+
+            <div class="uk-dropdown uk-dropdown-navbar submenu">
+                <ul class="uk-nav uk-nav-navbar">
+                    <li><a href="../parameters/pass.php">Passaggio di parametri</a></li>
+                    <li><a href="../parameters/menu.php">Menù dinamico</a></li>
+                </ul>
+            </div>
+
+        </li>
+        
+        <li class="uk-parent" data-uk-dropdown>
+            <a href="">Cookie e sessioni</a>
+
+            <div class="uk-dropdown uk-dropdown-navbar submenu">
+                <ul class="uk-nav uk-nav-navbar">
+                    <li><a href="../cookie_session/counter.php">Contatore</a></li>
+                    <li><a href="../cookie_session/login_cookie.php">Login (cookie)</a></li>
+                    <li><a href="../cookie_session/login_session.php">Login (session)</a></li>
+                </ul>
+            </div>
+        </li>
+        
+    </ul>
+
+</nav>
+
+                            
+
