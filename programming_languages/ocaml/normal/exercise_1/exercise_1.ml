2b7a95a28239e5d6ac41e209e23e06c40b3e47e6
Giovanni Santini <giovannisantini93@yahoo.it>
2016-04-04 11:49:52 +0200

    Initial attempt of doing exercises with Autotools

---
+++ programming_languages/ocaml/normal/exercise_1/exercise_1.ml
@@ -0,0 +1,18 @@
+(* For Printf.printf *)
+open Printf;;
+
+(* Insert code here... *)
+let alkaline_earth_metals = [
+	("beryllium",4);
+	("magnesium",12);
+	("calcium",20);
+	("strontium",38);
+	("barium",56);
+	("radium",88);
+]
+(*
+Our "main".
+*)
+let () =
+	Printf.printf "%s\n" "Hello world!"
+;;
