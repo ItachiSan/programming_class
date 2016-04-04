a210726ddde02d1a7d7ffa27991b9dfa614b9fb6
Giovanni Santini <giovannisantini93@yahoo.it>
2016-04-04 15:03:17 +0200

    OCaML exercise 1 finished

--- programming_languages/ocaml/normal/exercise_1/exercise_1.ml
+++ programming_languages/ocaml/normal/exercise_1/exercise_1.ml
@@ -1,7 +1,7 @@
 (* For Printf.printf *)
 open Printf;;
 
-(* Insert code here... *)
+(* Elements lists... *)
 let alkaline_earth_metals = [
 	("beryllium",4);
 	("magnesium",12);
@@ -9,10 +9,60 @@ let alkaline_earth_metals = [
 	("strontium",38);
 	("barium",56);
 	("radium",88);
-]
+];;
+
+let noble_gases = [
+	("helium",2);
+	("neon",10);
+	("argon",18);
+	("krypton",36);
+	("xenon",54);
+	("radon",86);
+];;
+
+(* Functions for the exercise *)
+(* Find the highest element *)
+let highest list =
+	let rec highest' list (name,num) =
+		match list with
+		| (s,x) :: lt when num > x 	-> highest' lt (name,num)
+		| (s,x) :: lt when num <= x	-> highest' lt (s,x)
+		| [] -> (name,num)
+	in
+		highest' list ("Dummy",0)
+;;
+
+(*
+Insertion sort for elements.
+Proudly copy-pasted from:
+http://ocaml.org/learn/taste.html
+and then adapted
+*)
+let rec sort list =
+	let rec insert ((a,b) as elem) list =
+		match list with
+		| [] -> [elem]
+		| (x,y) :: l when b < y 	-> elem :: (x,y) :: l
+		| (x,y) :: l when b >= y 	-> (x,y) :: insert elem l
+	in
+		match list with
+		| [] -> []
+		| x :: l -> insert x (sort l)
+;;
+
+(*
+Print all the elements in the elements list.
+*)
+let rec print_element_list list =
+	let print_element (a,b) = Printf.printf "%3d\t%s\n" b a in
+	match list with
+		| el :: lt 	-> print_element el; print_element_list lt
+		| [] 		-> ()
+;;
+
 (*
 Our "main".
 *)
 let () =
-	Printf.printf "%s\n" "Hello world!"
+	print_element_list(sort (alkaline_earth_metals @ noble_gases))
 ;;
